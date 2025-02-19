3229e07ef0594a19cb5a532fb9402247be61bb5f
Martin Schwaighofer <3856390+mschwaig@users.noreply.github.com>
2025-02-19 14:16:05 +0100

    adapt trust model so it builds for now

--- src/trace_signatures/verification/trust_model.py
+++ src/trace_signatures/verification/trust_model.py
@@ -4,12 +4,11 @@ from typing import Set
 from ..verification.verify_signatures import verify_trace_signatures
 
 from ..nix.types import (
-    ResolvedDerivation,
-    ResolvedOutput,
+    UnresolvedDerivation,
     ResolvedInputHash,
     UnresolvedInputHash,
-    TrustfullyResolvedDerivation,
-    TrustlesslyResolvedDerivation
+    TrustlesslyResolvedDerivation,
+    ContentHash
 )
 
 @dataclass (frozen=True)
@@ -21,16 +20,26 @@ class TrustedKey:
     #def contains_legacy_keys(self):
     #    return self.isLegacy
 
-    def ct_verify(self, resolved_drv: TrustlesslyResolvedDerivation, ct_signatures) -> Set[Set[Set[ResolvedOutput]]]:
+    def ct_verify(self, unresolved_drv: UnresolvedDerivation, ct_input_hash: ResolvedInputHash, resolution, ct_signatures) -> Set[Set[TrustlesslyResolvedDerivation]]:
         # ensure that signature is correctly signed with trusted key
         # and has the correct ct_input_hash
         # and then return the set of resolved derivations for which this holds
         # what we return here is basically {{drv1,drv2,drv3}} because they all belong to the same key
         # if the keys were different we would return {{drv1}, {drv2}, {drv3}}
-        output_mappings = verify_trace_signatures(self.key_bytes, ct_signatures, resolved_drv.input_hash)
-        typed_sets = set(map(lambda mapping: set(map(lambda output: ResolvedOutput(resolved_drv, resolved_drv.resolves.outputs[output], mapping[output]), mapping)), output_mappings))
+        output_mappings = verify_trace_signatures(self.key_bytes, ct_signatures, ct_input_hash)
+        # not sure if I can get rid of str(v) since v should already be a str
+        typed_mappings = list(map(lambda mapping: {unresolved_drv.outputs[k]: ContentHash(v) for k, v in mapping.items()}, output_mappings))
+        #typed_sets = set(map(lambda mapping: (map(lambda output: ResolvedOutput(resolved_drv, resolved_drv.resolves.outputs[output], mapping[output]), mapping)), output_mappings))
+        typed_sets = set(set(map(lambda mapping: TrustlesslyResolvedDerivation(
+            resolves=unresolved_drv,
+            input_hash=ct_input_hash,
+            inputs=resolution,
+            outputs=mapping
+        ), typed_mappings)))
 
-        return {typed_sets}
+        # TODO: get this to work
+
+        return set()
 
     #def dct_verify(self, dct_input_hash: UnresolvedInputHash, dct_signatures) -> Set[ResolvedDerivation]:
         # TODO: verify
@@ -59,7 +68,7 @@ class KeySetWithTreshold:
     #def contains_legacy_keys(self):
     #    return any(map(lambda x: x.contains_legacy_keys(), self.components))
 
-    def ct_verify(self, ct_input_hash: ResolvedInputHash, ct_signatures) -> Set[Set[ResolvedDerivation]]:
+    def ct_verify(self, ct_input_hash: ResolvedInputHash, ct_signatures) -> Set[Set[TrustlesslyResolvedDerivation]]:
         # group by signed output
         # do the sum thing for each output
         # actually scratch those two lines, we probably need to just gather all
@@ -68,7 +77,7 @@ class KeySetWithTreshold:
         #return sum(1 for x in self.components if x.ct_verify(ct_input_hash, ct_signatures)) >= self.treshold
         return set()
 
-    def dct_verify(self, dct_input_hash: UnresolvedInputHash, dct_signatures) ->Set[Set[ResolvedDerivation]]:
+    def dct_verify(self, dct_input_hash: UnresolvedInputHash, dct_signatures) ->Set[Set[TrustlesslyResolvedDerivation]]:
         # do the threshold thing for each combination of inputs
         #return sum(1 for x in self.components if x.dct_verify(dct_input_hash, dct_signatures)) >= self.treshold
         return set()
