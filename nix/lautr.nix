32fe8eac46626cacbd60d7c0674d5128078bef03
Martin Schwaighofer <3856390+mschwaig@users.noreply.github.com>
2025-05-06 20:14:18 +0200

    move rust code

--- nix/lautr.nix
+++ nix/lautr.nix
@@ -9,12 +9,12 @@ buildPythonPackage {
   pname = "lautr";
   version = "0.2.0";
 
-  src = ../datalog;
+  src = ../rust;
 
   pyproject = true;
 
   cargoDeps = rustPlatform.importCargoLock {
-    lockFile = ../datalog/Cargo.lock;
+    lockFile = ../rust/Cargo.lock;
     outputHashes = {
       "nix-compat-0.1.0" = "sha256-vSC6VxiRCtscfPuhejivMtinZXuj5tin9Mr7XW5cgJs=";
     };
