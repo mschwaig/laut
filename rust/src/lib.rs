dbffc3771864693f6ff9c4141460e2a44fe2f941
Martin Schwaighofer <3856390+mschwaig@users.noreply.github.com>
2025-05-07 13:22:39 +0200

    add notes and function signatures for datafrog implementation
    Wrote this some time ago already, adding it now, for reference.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,6 +9,71 @@ fn lautr(m: &Bound<'_, PyModule>) -> PyResult<()> {
     Ok(())
 }
 
+// let's use a hash map to store all of the actual hashes :D
+// insetad of mucking around with extracting the correct bits
+// from the inputs and storing them efficiently
+// this way we can feed in correctly formatted production data
+// but also abbreviated test data
+// and if we want, we can still do vaildation on the test data
+
+// we could also be ultra lazy and say we take hashing seriously and if hashes collide then we consider things to be the same
+// that way we can either do the hash function thing I just supposed, and assign increasing integers that way
+// or we can just use the hash values raw, if that is faster
+// we just cannot shorten them, because that makes us prone to collisions in the validation
+
+// I also like datafrog, because I think we could get nice error messages out if it
+// and maybe even a set of next actions if we are in the process of building stuff
+// and already specified the unresolved graph
+
+// it could tell us to fetch signatures
+// or build stuff
+// it could drive the build process
+
+#[pyfunction]
+fn setup() -> PyResult<usize> {
+}
+
+#[pyfunction]
+fn add_fod() -> PyResult<usize> {
+    // add to FOD relation
+}
+
+#[pyfunction]
+fn add_unresolved_derivation() -> PyResult<usize> {
+    // add to unresolved derivation relation
+}
+
+7#[pyfunction]
+fn add_resolved_derivation(resolves, with, resolving x with y) -> PyResult<usize> {
+   // add to resolved derivation relation
+}
+
+
+fn add_build_output_claim(resolved, built) -> PyResult<usize> {
+    // add to build output claim
+}
+
+#[pyfunction]
+fn add_target(build_output, trust_model) -> PyResult<usize> {
+    // add to build output claim
+}
+
+#[pyfunction]
+fn add_treshold(tm_name, treshold, constituents) -> PyResult<usize> {
+    // add to build output claim
+}
+
+#[pyfunction]
+fn compute_result() -> PyResult<usize> {
+// compute build output variable
+
+// if this a 1:1 relationship unification is total, or we reject builds - no frankenbuilds
+// if it is a 1:n relationship unification is partial, and we have to do rewriting
+
+// in parctice we could make unification only total in terms of runtime closures
+// and have it be ok if it is not total for build time closures
+}
+
 #[pyfunction]
 fn hash_upstream_placeholder(drv_path: &str, output_name: &str) -> PyResult<String> {
    return store_path::hash_upstream_placeholder("/nix/store/", drv_path, output_name)
