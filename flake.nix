26d33050479baefd022b0de671772bf57353512b
Martin Schwaighofer <mschwaig@users.noreply.github.com>
2025-05-09 01:25:40 +0200

    remove macOS build and flake utils for now
    Moving from souffle to datafrog actually raises the chances of macOS
    support coming back properly, since souffle was broken on macOS and
    I have high hopes for rust being easier to manage there.

--- flake.nix
+++ flake.nix
@@ -4,75 +4,74 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     bombon.url = "github:nikstur/bombon";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, bombon, flake-utils }@inputs:
-    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-darwin" ] (system:
+  outputs = { self, nixpkgs, bombon }@inputs:
     let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-        lib = pkgs.lib;
-        nix = pkgs.nix;
-        nix-vsbom = bombon.lib.${system}.buildBom nix {
-          includeBuildtimeDependencies = true;
-        };
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      lib = pkgs.lib;
+      nix = pkgs.nix;
+      nix-vsbom = bombon.lib.${system}.buildBom nix {
+        includeBuildtimeDependencies = true;
+      };
 
-        # TODO: do this using nix-instantiate like serge
-        test-drv-json = pkgs.writeShellScriptBin "examine-derivation" ''
-          cd ${nixpkgs.outPath}; ${pkgs.lix}/bin/nix derivation show --recursive -f . hello --arg config '{ contentAddressedByDefault = true; }'
+      # TODO: do this using nix-instantiate like serge
+      test-drv-json = pkgs.writeShellScriptBin "examine-derivation" ''
+        cd ${nixpkgs.outPath}; ${pkgs.lix}/bin/nix derivation show --recursive -f . hello --arg config '{ contentAddressedByDefault = true; }'
+      '';
+
+      scope = pkgs.callPackage ./default.nix { nixpkgs = null; };
+    in {
+      packages.${system} = {
+        inherit nix nix-vsbom test-drv-json;
+        inherit (scope) laut laut-sign-only lautr;
+        default = scope.laut;
+      };
+
+
+      checks.${system} = lib.filterAttrs (name: value:
+        # since AND for trust models is not implemented yet
+        # we only run one small and one large text
+        # using builderA OR builderB as the trust model
+        (name == "small") || (name == "large"))
+        (import ./vm-tests {
+          pkgsIA = pkgs;
+          inherit nixpkgs;
+          inherit (scope) laut;
+        });
+
+      devShell.${system} = let
+        pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+          rfc8785
+          pyjwt
+          cryptography
+          boto3
+          click
+          loguru
+          #sigstore
+          pytest
+          debugpy
+          memory_profiler
+        ] ++ [
+          pkgs.pyright
+        ] ++ [
+          scope.lautr
+        ]);
+      in pkgs.mkShell {
+        shellHook = ''
+          export PATH=${lib.makeBinPath [ pkgs.diffoscope ]}:$PATH
         '';
 
-        scope = pkgs.callPackage ./default.nix { nixpkgs = null; };
-    in {
-        packages = {
-          inherit nix nix-vsbom test-drv-json;
-          inherit (scope) laut laut-sign-only lautr;
-          default = scope.laut;
-        };
-
-
-        checks = lib.filterAttrs (name: value:
-          # since AND for trust models is not implemented yet
-          # we only run one small and one large text
-          # using builderA OR builderB as the trust model
-          (name == "small") || (name == "large"))
-          (import ./vm-tests {
-            pkgsIA = pkgs;
-            inherit nixpkgs;
-            inherit (scope) laut;
-          });
-
-        devShell = let
-          pythonEnv = pkgs.python3.withPackages (ps: with ps; [
-            rfc8785
-            pyjwt
-            cryptography
-            boto3
-            click
-            loguru
-            #sigstore
-            pytest
-            debugpy
-            memory_profiler
-          ] ++ [
-            pkgs.pyright
-          ] ++ [
-            scope.lautr
-          ]);
-        in pkgs.mkShell {
-          shellHook = ''
-            export PATH=${lib.makeBinPath [ pkgs.diffoscope ]}:$PATH
-          '';
-
-          PYTEST_FOR_VSCODE = "${pythonEnv}/bin/pytest";
-          buildInputs = [
-            pkgs.cargo
-            pkgs.rustc
-            pythonEnv
-            pkgs.souffle
-          ];
-        };
-    });
+        PYTEST_FOR_VSCODE = "${pythonEnv}/bin/pytest";
+        buildInputs = [
+          pkgs.cargo
+          pkgs.rustc
+          pythonEnv
+          pkgs.souffle
+        ];
+      };
+  };
 }
