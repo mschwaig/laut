533ca0c4798be02166364291086f8de8147de7bf
Martin Schwaighofer <3856390+mschwaig@users.noreply.github.com>
2025-05-13 20:31:35 +0200

    fix build by figuring out the protobuf parts
    * protobuf python package was shadowing pkgs.protobuf
    * we need another copy of SNIX as PROTO_ROOT

--- nix/lautr.nix
+++ nix/lautr.nix
@@ -1,35 +1,39 @@
 {
+  pkgs,
+  fetchgit,
   buildPythonPackage,
-  protobuf,
-  python,
-  pytestCheckHook,
   rustPlatform,
 }:
-
-buildPythonPackage {
-  pname = "lautr";
-  version = "0.2.0";
-
-  src = ../rust;
-
-  pyproject = true;
-
-  PROTOC = "${protobuf}/bin/protoc-29.4.0";
-
-  cargoDeps = rustPlatform.importCargoLock {
-    lockFile = ../rust/Cargo.lock;
-    outputHashes = {
-      "laut-compat-0.1.0" = "sha256-KjiJNlbQA2KhyVFTSVLA4P31WIx8Fyy3jn+WwBJWE+4=";
-      "wu-manber-0.1.0" = "sha256-7YIttaQLfFC/32utojh2DyOHVsZiw8ul/z0lvOhAE/4=";
-    };
+let
+  snix-hash = "sha256-KjiJNlbQA2KhyVFTSVLA4P31WIx8Fyy3jn+WwBJWE+4=";
+  snix = fetchgit {
+    url = "https://github.com/mschwaig/snix";
+    rev = "4311236e67a0026b946d4d509aafac18037721d7";
+    hash = snix-hash;
   };
+in
+  buildPythonPackage {
+    pname = "lautr";
+    version = "0.2.0";
 
-  nativeBuildInputs = with rustPlatform; [
-    cargoSetupHook
-    maturinBuildHook
-    protobuf
-  ];
+    src = ../rust;
 
+    pyproject = true;
+    cargoDeps = rustPlatform.importCargoLock {
+      lockFile = ../rust/Cargo.lock;
+      outputHashes = {
+        "laut-compat-0.1.0" = snix-hash;
+        "wu-manber-0.1.0" = "sha256-7YIttaQLfFC/32utojh2DyOHVsZiw8ul/z0lvOhAE/4=";
+      };
+    };
 
-  pythonImportsCheck = [ "lautr" ];
-}
+    PROTO_ROOT = snix;
+
+    nativeBuildInputs = with rustPlatform; [
+      cargoSetupHook
+      maturinBuildHook
+      pkgs.protobuf
+    ];
+
+    pythonImportsCheck = [ "lautr" ];
+  }
